ad868ba9f4a55a4df2da7dd85b41e557e4c60ce8
eCrMarques <126702522+eCrMarques@users.noreply.github.com>
2023-10-19 19:45:24 -0300

    Fixing Angular-Blog
    Fixing ng serve error

---
+++ Angular-Blog/src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake =[
+    {
+        "id":"0",
+        "title":"Wanda Continuação",
+        "description":'"Uma continuação de "WandaVision" promete explorar o legado de seus filhos, a complexa relação de Wanda e Visão, e a expansão do Multiverso da Marvel, ansiosamente aguardada pelos fãs."',
+        "photoCover":"https://lumiere-a.akamaihd.net/v1/images/wanda_maximoff_profile_2994bdb4.jpeg?region=64,0,712,400"
+    },
+    {
+        "id":"1",
+        "title":"Novo Homem-Aranha",
+        "description":"O novo Homem-Aranha traz um jovem herói, Miles Morales, à vanguarda. Com poderes únicos e uma jornada de autodescoberta, ele protege Nova York em um emocionante universo animado.",
+        "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2023/07/Destaque-Homem-Aranha.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Homem de Ferro: O Último Ato - Tony Stark Encara Seu Destino",
+        "description":"Homem de Ferro: Ultimato é o épico final da saga de Tony Stark, enquanto ele enfrenta o destino, sacrifícios e o vilão Thanos em um confronto decisivo para o universo.",
+        "photoCover":"https://epipoca.com.br/wp-content/uploads/2023/10/tony-stark-vingadores-ultimato.jpg"
+    },
+    {
+        "id":"3",
+        "title":"Superman Salva Metrópolis de uma Ameaça Cósmica Inédita!",
+        "description":"Superman, o último filho de Krypton, enfrenta uma ameaça cósmica nunca antes vista, usando seus poderes extraordinários para proteger Metrópolis e a Terra de uma catástrofe iminente.",
+        "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/07/vitrine_minhas_aventuras_com_o_superman_trailer_e_sinopse_nerdbunker__v907a0ket-1210x544.jpg"
+    },
+    {
+        "id":"4",
+        "title":"Mulher-Maravilha Enfrenta Novos Desafios em Missão Internacional de Paz",
+        "description":"A Mulher-Maravilha, Diana Prince, encara desafios globais em uma missão internacional de paz, usando sua força e compaixão para enfrentar ameaças e promover a harmonia em todo o mundo.",
+        "photoCover":"https://s3.amazonaws.com/blog.dentrodahistoria.com.br/wp-content/uploads/2021/10/19174636/thumb_aniversario_mulher_maravilha-800x500.jpg"
+    }
+]
